fixed masking

transferLearning_MoleculeNet_PPI/chem/finetune_link.py
@@ -100,7 +100,7 @@ def eval(args, kwargs, target, model, device, loader):
 def main(**kwargs):
     # Training settings
     parser = argparse.ArgumentParser(description='PyTorch implementation of pre-training of graph neural networks')
-    parser.add_argument('--device', type=int, default=1,
+    parser.add_argument('--device', type=int, default=0,
                         help='which gpu to use if any (default: 0)')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='input batch size for training (default: 32)')
@@ -123,7 +123,7 @@ def main(**kwargs):
     parser.add_argument('--JK', type=str, default="last",
                         help='how the node features across layers are combined. last, sum, max or concat')
     parser.add_argument('--gnn_type', type=str, default="gin")
-    parser.add_argument('--dataset', type=str, default = 'tox21', help='root directory of dataset. For now, only classification.')
+    parser.add_argument('--dataset', type=str, default = 'clintox', help='root directory of dataset. For now, only classification.')
     parser.add_argument('--gnn_model_file', type=str, default = 'new_models_graphcl/graphcl.pth', help='filename to read the gnn model (if there is any)')
     parser.add_argument('--proj_head_file', type=str, default = 'new_models_graphcl/graphcl_head.pth', help='filename to read the projection head weights')
     parser.add_argument('--filename', type=str, default = '', help='output filename')
@@ -134,6 +134,7 @@ def main(**kwargs):
     parser.add_argument('--num_workers', type=int, default = 4, help='number of workers for dataset loading')
     args = parser.parse_args()
 
+    print(args, kwargs)
 
     torch.manual_seed(args.runseed)
     np.random.seed(args.runseed)
@@ -307,4 +308,4 @@ def main(**kwargs):
 
 if __name__ == "__main__":
     for _ in range(10):
-        main(num_clusters=70, ortho_weight=9.75e-5)
+        main(num_clusters=60, ortho_weight=7.5e-5)

transferLearning_MoleculeNet_PPI/chem/finetune_mask.py
@@ -134,7 +134,7 @@ def main(**kwargs):
     parser.add_argument('--JK', type=str, default="last",
                         help='how the node features across layers are combined. last, sum, max or concat')
     parser.add_argument('--gnn_type', type=str, default="gin")
-    parser.add_argument('--dataset', type=str, default = 'bbbp-mask', help='root directory of dataset. For now, only classification.')
+    parser.add_argument('--dataset', type=str, default = 'tox21-mask', help='root directory of dataset. For now, only classification.')
     parser.add_argument('--gnn_model_file', type=str, default = 'new_model_gin/masking.pth', help='filename to read the gnn model (if there is any)')
     parser.add_argument('--proj_head_file', type=str, default = 'new_model_gin/masking_atom_head.pth', help='filename to read the projection head weights')
     parser.add_argument('--filename', type=str, default = '', help='output filename')
@@ -265,9 +265,9 @@ def main(**kwargs):
 
             atom_feats = torch.vstack((atom_list_0, atom_list_1)).to(device)
             atom_feats = F.normalize(atom_feats, dim=-1)
-            offset = torch.zeros(atom_feats.shape).to(device)
-            torch.nn.init.constant_(offset, torch.finfo(torch.float).tiny)
-            atom_feats += offset
+            #offset = torch.zeros(atom_feats.shape).to(device)
+            #torch.nn.init.constant_(offset, torch.finfo(torch.float).tiny)
+            #atom_feats += offset
 
 
         if not (args.gnn_model_file == "" or args.proj_head_file == ""):
@@ -324,7 +324,7 @@ def main(**kwargs):
 
         avg_val_acc = sum(avg_val_acc) / len(avg_val_acc)
         total_val_acc.append(avg_val_acc)
-        print("val: %f, test: %f" %(avg_val_acc, ass_test_acc))
+        print(ass_test_acc)
     return sum(total_val_acc) / len(total_val_acc)
 
 if __name__ == "__main__":